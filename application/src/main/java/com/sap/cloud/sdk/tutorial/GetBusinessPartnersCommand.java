c7d2285c76db37b146b0c84dcec3f27daa84620f
kyoneo <kenji.yoneo@sap.com>
2021-10-16 00:30:11 +0900

    add Unittest

--- application/src/main/java/com/sap/cloud/sdk/tutorial/GetBusinessPartnersCommand.java
+++ application/src/main/java/com/sap/cloud/sdk/tutorial/GetBusinessPartnersCommand.java
@@ -1,92 +1,45 @@
 package com.sap.cloud.sdk.tutorial;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.time.Duration;
-import java.util.Collections;
 import java.util.List;
 
+import com.sap.cloud.sdk.cloudplatform.connectivity.HttpDestination;
 import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceConfiguration;
 import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceDecorator;
-import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceIsolationMode;
 import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceRuntimeException;
 import com.sap.cloud.sdk.datamodel.odata.client.exception.ODataException;
-import com.sap.cloud.sdk.datamodel.odata.helper.Order;
 
-import com.sap.cloud.sdk.s4hana.connectivity.ErpHttpDestination;
-import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.AddressEmailAddress;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
-import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartnerAddress;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.services.DefaultBusinessPartnerService;
 
 public class GetBusinessPartnersCommand {
-    private static final Logger logger = LoggerFactory.getLogger(GetBusinessPartnersCommand.class);
-    // TODO: uncomment the lines below and insert your API key, if you are using the sandbox service
-    // private static final String APIKEY_HEADER = "apikey";
-    // private static final String SANDBOX_APIKEY = "";
-    private static final String CATEGORY_PERSON = "1";
-    private final ErpHttpDestination destination;
 
     private final BusinessPartnerService businessPartnerService;
-    private final ResilienceConfiguration myResilienceConfig;
+    private final HttpDestination httpDestination;
 
-    public GetBusinessPartnersCommand(ErpHttpDestination destination) {
+    public GetBusinessPartnersCommand(HttpDestination destination) {
         this(destination, new DefaultBusinessPartnerService());
     }
 
-    public GetBusinessPartnersCommand(ErpHttpDestination destination, BusinessPartnerService service) {
-        this.destination = destination;
-        businessPartnerService = service;
-
-        myResilienceConfig = ResilienceConfiguration.of(BusinessPartnerService.class)
-                .isolationMode(ResilienceIsolationMode.TENANT_AND_USER_OPTIONAL)
-                .timeLimiterConfiguration(
-                        ResilienceConfiguration.TimeLimiterConfiguration.of()
-                                .timeoutDuration(Duration.ofMillis(10000)))
-                .bulkheadConfiguration(
-                        ResilienceConfiguration.BulkheadConfiguration.of()
-                                .maxConcurrentCalls(20));
-
-        final ResilienceConfiguration.CacheConfiguration cacheConfig =
-            ResilienceConfiguration.CacheConfiguration
-                .of(Duration.ofSeconds(10))
-                .withoutParameters();
-
-        myResilienceConfig.cacheConfiguration(cacheConfig);                         
+    public GetBusinessPartnersCommand(HttpDestination httpDestination, BusinessPartnerService businessPartnerService) {
+        this.businessPartnerService = businessPartnerService;
+        this.httpDestination = httpDestination;
     }
 
     public List<BusinessPartner> execute() {
-        return ResilienceDecorator.executeSupplier(this::run, myResilienceConfig, e -> {
-            logger.warn("Fallback called because of exception.", e);
-            return Collections.emptyList();
-        });
+        return ResilienceDecorator.executeSupplier(this::run, ResilienceConfiguration.of(GetBusinessPartnersCommand.class));
     }
 
     private List<BusinessPartner> run() {
+
         try {
-            return businessPartnerService
-                    .getAllBusinessPartner()
-                    .select(BusinessPartner.BUSINESS_PARTNER,
-                            BusinessPartner.LAST_NAME,
-                            BusinessPartner.FIRST_NAME,
-                            BusinessPartner.IS_MALE,
-                            BusinessPartner.IS_FEMALE,
-                            BusinessPartner.CREATION_DATE,
-                            BusinessPartner.TO_BUSINESS_PARTNER_ADDRESS
-                                    .select(BusinessPartnerAddress.CITY_NAME,
-                                            BusinessPartnerAddress.COUNTRY,
-                                            BusinessPartnerAddress.TO_EMAIL_ADDRESS
-                                                    .select(AddressEmailAddress.EMAIL_ADDRESS)
-                                    )
-                    )
-                    .filter(BusinessPartner.BUSINESS_PARTNER_CATEGORY.eq(CATEGORY_PERSON))
-                    .orderBy(BusinessPartner.LAST_NAME, Order.ASC)
-                    .top(200)
-                    // TODO: uncomment the line below, if you are using the sandbox service
-                    // .withHeader(APIKEY_HEADER, SANDBOX_APIKEY)
-                    .executeRequest(destination);
+
+            return businessPartnerService.getAllBusinessPartner()
+                    .filter(BusinessPartner.CUSTOMER.ne(""))
+                    .select(BusinessPartner.FIRST_NAME,
+                            BusinessPartner.LAST_NAME)
+                    .executeRequest(httpDestination);
+
         } catch (ODataException e) {
             throw new ResilienceRuntimeException(e);
         }
---
+++ application/src/test/java/com/sap/cloud/sdk/tutorial/GetBusinessPartnerMockedTest.java
@@ -0,0 +1,82 @@
+package com.sap.cloud.sdk.tutorial;
+
+import com.sap.cloud.sdk.cloudplatform.connectivity.HttpDestination;
+import com.sap.cloud.sdk.datamodel.odata.client.exception.ODataException;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.selectable.BusinessPartnerSelectable;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceRuntimeException;
+import com.google.common.collect.Lists;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+
+public class GetBusinessPartnerMockedTest {
+    private static BusinessPartner alice;
+    private static BusinessPartner bob;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+
+        alice = new BusinessPartner();
+        alice.setFirstName("Alice");
+
+        bob = new BusinessPartner();
+        bob.setFirstName("Bob");
+    }
+
+    @Test
+    public void testGetAnyBusinessPartner() throws Exception {
+        final BusinessPartnerService service = Mockito.mock(BusinessPartnerService.class, RETURNS_DEEP_STUBS);
+        final HttpDestination httpDestination = Mockito.mock(HttpDestination.class);
+
+        when(service.getAllBusinessPartner()
+                .filter(any())
+                .select(any())
+                .executeRequest(any()))
+                .thenReturn(Lists.newArrayList(alice, bob));
+
+        final List<BusinessPartner> businessPartnerList = new GetBusinessPartnersCommand(httpDestination, service).execute();
+
+        assertEquals(2, businessPartnerList.size());
+        assertEquals("Alice", businessPartnerList.get(0).getFirstName());
+        assertEquals("Bob", businessPartnerList.get(1).getFirstName());
+    }
+
+    @Test
+    public void testGetSpecificBusinessPartner() throws Exception {
+        final BusinessPartnerService service = Mockito.mock(BusinessPartnerService.class, RETURNS_DEEP_STUBS);
+        final HttpDestination httpDestination = Mockito.mock(HttpDestination.class);
+        when(service.getAllBusinessPartner()
+                .filter(BusinessPartner.CUSTOMER.ne(""))
+                .select(any(BusinessPartnerSelectable.class))
+                .executeRequest(any(HttpDestination.class)))
+                .thenReturn(Lists.newArrayList(alice));
+
+        final List<BusinessPartner> businessPartnerList = new GetBusinessPartnersCommand(httpDestination, service).execute();
+
+        assertEquals(1, businessPartnerList.size());
+        assertEquals("Alice", businessPartnerList.get(0).getFirstName());
+    }
+
+    @Test(expected = ResilienceRuntimeException.class)
+    public void testGetBusinessPartnerFailure() throws Exception {
+        final BusinessPartnerService service = Mockito.mock(BusinessPartnerService.class, RETURNS_DEEP_STUBS);
+        final HttpDestination httpDestination = Mockito.mock(HttpDestination.class);
+
+        when(service.getAllBusinessPartner()
+                .filter(any())
+                .select(any())
+                .executeRequest(any()))
+                .thenThrow(new ODataException(null, "Something went wrong", null));
+
+        new GetBusinessPartnersCommand(httpDestination, service).execute();
+    }
+}
